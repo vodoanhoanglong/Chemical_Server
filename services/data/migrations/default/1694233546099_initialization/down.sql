821b36e33dd93fda416a0bfe463afa82eb909532
vodoanhoanglong <vodoanhoanglong10a9@gmail.com>
2024-03-16 20:53:22 +0700

    finished something api

--- services/data/migrations/default/1694233546099_initialization/down.sql
+++ services/data/migrations/default/1694233546099_initialization/down.sql
@@ -2,6 +2,10 @@ DROP INDEX  system_user_phone_unique;
 DROP INDEX  system_user_email_unique; 
 DROP INDEX  system_user_identity_unique; 
 
+DROP TABLE "public"."notification_reads";
+DROP TABLE "public"."notifications";
+DROP TABLE "public"."shared_videos";
+DROP TABLE "public"."video_votes";
 DROP TABLE "public"."system_users";
 
 
--- services/data/migrations/default/1694233546099_initialization/up.sql
+++ services/data/migrations/default/1694233546099_initialization/up.sql
@@ -84,3 +84,91 @@ CREATE UNIQUE INDEX system_user_phone_unique ON system_users("phoneNumber") WHER
 CREATE UNIQUE INDEX system_user_email_unique ON system_users("email") WHERE (status = 'active');
 CREATE UNIQUE INDEX system_user_identity_unique ON system_users("identityCard") WHERE (status = 'active');
 
+CREATE TABLE "public"."shared_videos"
+(
+    "id"              uuid        NOT NULL DEFAULT gen_random_uuid(),
+    "createdAt"       timestamptz NOT NULL DEFAULT now(),
+    "updatedAt"       timestamptz NOT NULL DEFAULT now(),
+    "createdBy"       uuid references "public"."system_users" ("id") on update restrict on delete restrict,
+    "updatedBy"       uuid references "public"."system_users" ("id") on update restrict on delete restrict,
+    "status"          text        NOT NULL DEFAULT 'active',
+    "url"             text,
+    "title"           text,
+    "description"     text,
+    PRIMARY KEY ("id")
+);
+
+CREATE TRIGGER "set_public_shared_videos_updatedAt"
+    BEFORE UPDATE
+    ON "public"."shared_videos"
+    FOR EACH ROW
+EXECUTE PROCEDURE "public"."set_current_timestamp_updatedAt"();
+COMMENT ON TRIGGER "set_public_shared_videos_updatedAt" ON "public"."shared_videos"
+    IS 'trigger to set value of column "updatedAt" to current timestamp on row update';
+
+
+CREATE TABLE "public"."video_votes"
+(
+    "id"              uuid        NOT NULL DEFAULT gen_random_uuid(),
+    "createdAt"       timestamptz NOT NULL DEFAULT now(),
+    "status"          text        NOT NULL DEFAULT 'active',
+    "videoId"         uuid references "public"."shared_videos" ("id") on update restrict on delete restrict,
+    "userId"          uuid references "public"."system_users" ("id") on update restrict on delete restrict,
+    "react"           text        NOT NULL DEFAULT 'voted',
+    PRIMARY KEY ("id")
+);
+
+CREATE UNIQUE INDEX video_vote_userId_videoId_unique ON video_votes("userId", "videoId") WHERE (status = 'active');
+
+
+CREATE TABLE "public"."notifications"
+(
+    "id"                    uuid        NOT NULL DEFAULT gen_random_uuid(),
+    "createdAt"             timestamptz NOT NULL DEFAULT now(),
+    "updatedAt"             timestamptz NOT NULL DEFAULT now(),
+    "createdBy"             uuid,
+    "updatedBy"             uuid,
+    "status"                text        NOT NULL DEFAULT 'active',
+    "title"                 text        NOT NULL,
+    "subtitle"              text,  
+    "content"               text        NOT NULL,
+    PRIMARY KEY ("id"),
+    FOREIGN KEY ("createdBy") REFERENCES "public"."system_users" ("id") ON UPDATE restrict ON DELETE restrict,
+    FOREIGN KEY ("updatedBy") REFERENCES "public"."system_users" ("id") ON UPDATE restrict ON DELETE restrict
+);
+
+CREATE TRIGGER "set_public_notifications_updatedAt"
+    BEFORE UPDATE
+    ON "public"."notifications"
+    FOR EACH ROW
+EXECUTE PROCEDURE "public"."set_current_timestamp_updatedAt"();
+COMMENT ON TRIGGER "set_public_notifications_updatedAt" ON "public"."notifications"
+    IS 'trigger to set value of column "updatedAt" to current timestamp on row update';
+
+CREATE TABLE "public"."notification_reads"
+(
+    "id"                    uuid        NOT NULL DEFAULT gen_random_uuid(),
+    "userId"                uuid,
+    "notificationId"        uuid,
+    "readAt"                timestamptz,
+    PRIMARY KEY ("id"),
+    FOREIGN KEY ("userId") REFERENCES "public"."system_users" ("id") ON UPDATE restrict ON DELETE restrict,
+    FOREIGN KEY ("notificationId") REFERENCES "public"."notifications" ("id") ON UPDATE restrict ON DELETE restrict
+);
+
+
+CREATE OR REPLACE FUNCTION public.push_notify()
+ RETURNS trigger
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+    INSERT INTO notification_reads ("userId", "notificationId")
+    SELECT id, NEW.id FROM system_users;
+    RETURN NEW;
+END;
+$function$;
+
+CREATE TRIGGER push_notify_after_insert
+    AFTER INSERT ON notifications
+    FOR EACH ROW
+    EXECUTE PROCEDURE push_notify();
